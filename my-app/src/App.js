b6cf856a8d5950443ff4eeeca62503adcb1d81a1
NickPavlovskii <84456340+NickPavlovskii@users.noreply.github.com>
2022-11-04 18:23:19 +0300

    /// ...

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,10 @@ import './style/style.css';
 
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
-import Home from './Home/Home';
+import Home from './Page/Home/Home';
 import About from './Page/Abaut/Abaut';
 
-
+import Contact from './Page/Contact/Contact';
 
 
 
@@ -28,6 +28,8 @@ function App() {
     <Routes>
         <Route exact path='/'  element={<Home />} />
         <Route path='/about' element={<About/>} />
+
+        <Route path='/contact' element={< Contact/>} />
        
     </Routes>
     </Router>
---
+++ my-app/src/Page/Abaut/AboutComp/AboutApp/AboutApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './AboutApp.css'
+export const AboutApp = () => {
+  return (
+    <section id="About-app" className='section-p1' >
+        <hr className='hr' />
+            <h1>Dowload Our <a href="#"  >App</a> </h1>
+            <div className='video'>
+                <video autoPlay muted src="../../../../img/about/1.mp4"></video>
+                 </div>
+                
+</section>
+  )
+}
+export default AboutApp;
